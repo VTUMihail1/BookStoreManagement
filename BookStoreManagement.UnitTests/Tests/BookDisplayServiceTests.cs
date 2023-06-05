3b896439257ed83ec31cb8407e97f60c5e312c96
Mihail <vtumihail@gmail.com>
2023-06-05 17:19:42 +0300

    Added tests for the CalculateDiscountService class

--- BookStoreManagement.UnitTests/Tests/BookDisplayServiceTests.cs
+++ BookStoreManagement.UnitTests/Tests/BookDisplayServiceTests.cs
@@ -11,23 +11,23 @@ namespace BookStoreManagement.UnitTests.Tests
     public class BookDisplayServiceTests
     {
         private IBookDisplayService _bookDisplayService;
-        private Mock<IBookFileService> _bookFileService;
-        private Mock<IGetKeywordService> _getKeywordService;
+        private Mock<IBookFileService> _mockBookFileService;
+        private Mock<IGetKeywordService> _mockGetKeywordService;
 
         [SetUp]
         public void Setup()
         {
             var bookList = new List<IBook>() { new Book() { Author = "Vazov", Title = "Pod igoto" } };
-            _getKeywordService = new Mock<IGetKeywordService>();
-            _bookFileService = new Mock<IBookFileService>();
-            _bookFileService.Setup(x => x.GetAllBooks()).Returns(bookList);
-            _bookDisplayService = new BookDisplayService(_bookFileService.Object, _getKeywordService.Object);
+            _mockGetKeywordService = new Mock<IGetKeywordService>();
+            _mockBookFileService = new Mock<IBookFileService>();
+            _mockBookFileService.Setup(x => x.GetAllBooks()).Returns(bookList);
+            _bookDisplayService = new BookDisplayService(_mockBookFileService.Object, _mockGetKeywordService.Object);
         }
 
         [Test]
         public void DisplaySearchedBooks_AuthorMatchesTheKeyword_ReturnsSearchedBooks()
         {
-            _getKeywordService.Setup(x => x.GetKeyword()).Returns("Vazov");
+            _mockGetKeywordService.Setup(x => x.GetKeyword()).Returns("Vazov");
 
             var list = _bookDisplayService.DisplaySearchedBooks();
 
@@ -37,7 +37,7 @@ namespace BookStoreManagement.UnitTests.Tests
         [Test]
         public void DisplaySearchedBooks_AuthorDoesNotMatchesTheKeyword_ReturnsEmptyList()
         {
-            _getKeywordService.Setup(x => x.GetKeyword()).Returns("Vazovv");
+            _mockGetKeywordService.Setup(x => x.GetKeyword()).Returns("Vazovv");
 
             var list = _bookDisplayService.DisplaySearchedBooks();
 
@@ -47,7 +47,7 @@ namespace BookStoreManagement.UnitTests.Tests
         [Test]
         public void DisplaySearchedBooks_TitleMatchesTheKeyword_ReturnsSearchedBooks()
         {
-            _getKeywordService.Setup(x => x.GetKeyword()).Returns("Pod igoto");
+            _mockGetKeywordService.Setup(x => x.GetKeyword()).Returns("Pod igoto");
 
             var list = _bookDisplayService.DisplaySearchedBooks();
 
@@ -57,7 +57,7 @@ namespace BookStoreManagement.UnitTests.Tests
         [Test]
         public void DisplaySearchedBooks_TitleDoesNotMatchesTheKeyword_ReturnsEmptyList()
         {
-            _getKeywordService.Setup(x => x.GetKeyword()).Returns("Pod igotoo");
+            _mockGetKeywordService.Setup(x => x.GetKeyword()).Returns("Pod igotoo");
 
             var list = _bookDisplayService.DisplaySearchedBooks();
 
---
+++ BookStoreManagement.UnitTests/Tests/CalculateDiscountServiceTests.cs
@@ -0,0 +1,52 @@
+﻿using BookStoreManagement.Interfaces.Services;
+using BookStoreManagement.Logic.Services;
+using BookStoreManagement.StaticData.StaticData;
+using NUnit.Framework;
+
+namespace BookStoreManagement.UnitTests.Tests
+{
+    [TestFixture]
+    public class CalculateDiscountServiceTests
+    {
+        private ICalculateDiscountService _calculateDiscountService;
+
+        [SetUp]
+        public void SetUp()
+        {
+            _calculateDiscountService = new CalculateDiscountService();
+        }
+
+        [Test]
+        public void Calculate_PriceIsLowerThanFiftheen_ReturnsFivePercent()
+        {
+            int value = 10;
+
+            decimal result = _calculateDiscountService.Calculate(value);
+
+            decimal newDiscount = result / value;
+            Assert.That(newDiscount, Is.EqualTo(BookStoreStaticData.LowestPercentageDiscount));
+        }
+
+        [Test]
+        public void Calculate_PriceIsHigherThanFourteenLowerThanTwentySix_ReturnsTenPercent()
+        {
+            decimal value = 20;
+
+            decimal result = _calculateDiscountService.Calculate(value);
+
+            decimal newDiscount = result / value;
+            Assert.That(newDiscount, Is.EqualTo(BookStoreStaticData.MiddlePercentageDiscount));
+        }
+
+        [Test]
+        public void Calculate_PriceIsHigherThanTwentyFive_ReturnsFiftheenPercent()
+        {
+            int value = 50;
+
+            decimal result = _calculateDiscountService.Calculate(value);
+
+            decimal newDiscount = result / value;
+            Assert.That(newDiscount, Is.EqualTo(BookStoreStaticData.HighestPercentageDiscount));
+        }
+    }
+}
